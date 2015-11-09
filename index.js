3c3aa2d7181bc886e77a4e3c970bbeb0d3596367
Jeremy Stover(Kadajett) <jeremy.ryan.stover@gmail.com>
2015-11-09 11:24:44 -0800

    added some code to make sure the connection to the database happens, which was the 'get posts' bug

--- index.js
+++ index.js
@@ -2,44 +2,70 @@ var express = require('express');
 var mongoose = require('mongoose');
 var _ = require('lodash');
 var bodyParser = require('body-parser');
-
-
-
 var Post = require('./models/Post');
-var prompt = require('prompt');
-
-var api = {},
-  app = express();
-
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
 
 
-// TODO Make route to show all posts.
-// TODO Make route to sort posts by keywords
-// TODO Make route to get all keywords in posts.
 
-api.middleWare = {
-  auth: function(req, res, next) {
-    // TODO Put real auth code here.
-    console.log('Authed');
-    next();
+var god = (function() {
+  var server = {},
+    api = {},
+    app = express(),
+    db = {};
+
+  api.middleWare = {
+    auth: function(req, res, next) {
+      // TODO Put real auth code here.
+      console.log('Authed');
+      next();
+    }
+  };
+
+  function init() {
+    dbConnect();
+    setupMiddleware();
+
   }
-};
 
-var router = express.Router();
+  function dbConnect() {
+    mongoose.connect('mongodb://localhost/test');
+    db = mongoose.connection;
+    dbOn();
+  }
+
+  function setupMiddleware() {
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+    app.use(bodyParser.json());
+    app.use(api.middleWare.auth);
+    // app.use(api.middleWare.auth);
+    app.use(Post.Post().init(express, mongoose));
+  }
+  // TODO Make route to show all posts.
+  // TODO Make route to sort posts by keywords
+  // TODO Make route to get all keywords in posts.
+
+
+  // var router = express.Router();
 
 
 
-router.use(api.middleWare.auth);
-// Add the Post Routes
-app.use(Post.Post().init(express, mongoose));
-// app.use(router);
-var server = app.listen(3000, function() {
-  var host = server.address().address;
-  var port = server.address().port;
+  // router.
+  // Add the Post Routes
 
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+  // app.use(router);
+  function dbOn(){
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function(callback) {
+      server = app.listen(3000, function() {
+        var host = server.address().address;
+        var port = server.address().port;
+
+        console.log('Example app listening at http://localhost:3000');
+      });
+    });
+  }
+
+
+  init();
+}());
